cd1403c7bb997cead40c152707f2fc4c99fdefae
JonathanLebourg <jonjonlebourg@gmail.com>
2018-10-09 14:19:24 +0200

    mis en page

--- exercice3.js
+++ exercice3.js
@@ -1,2 +1,14 @@
-var Nom=prompt("Bonjour, tapez votre prénom");
-alert(Nom);
+var nom=prompt("tapez votre prénom");
+
+function prenom(nom){
+ var regex= /^[a-zA-ZÀ-ÿ]+$/; //définition des caracrtères acceptés
+
+if (!regex.test(nom)||nom==null){ //test du regex
+   alert("Problème de saisie");
+var saisie= prompt("réessayez..."); //nouvelle saisie pour
+   prenom(saisie);}
+else{
+   alert("Bonjour, "+nom+" !");} //réponse
+}
+
+prenom(nom);
